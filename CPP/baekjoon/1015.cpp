b4c9de4ea1aa329182a20b0d8c30f3bce87ade50
Heejun-0219 <heejun0219@gmail.com>
2023-05-10 00:08:12 +0900

    algo

---
+++ CPP/baekjoon/1015.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+vector<int> selectionSort(const vector<int> &arr) {
+    int N = arr.size();
+    vector<int> P(N);
+    vector<bool> used(N);
+
+    for (int i = 0; i < N; i++) {
+        int min_val = INT_MAX;
+        int min_idx = -1;
+
+        for (int j = 0; j < N; j++) {
+            if (!used[j] && arr[j] < min_val) {
+                min_val = arr[j];
+                min_idx = j;
+            }
+        }
+
+        P[min_idx] = i;
+        used[min_idx] = true;
+    }
+
+    return P;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    srand(time(0));
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        A[i] = rand() % 101;
+        cout << A[i] << " ";
+    }
+    cout << endl << endl;
+
+    vector<int> P = selectionSort(A);
+
+    for (int i = 0; i < N; i++) {
+        cout << P[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ CPP/baekjoon/1015_2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+void mySwap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+struct Element {
+    int index;
+    int value;
+};
+
+bool compareValues(const Element &a, const Element &b) {
+    if (a.value == b.value) {
+        return a.index < b.index;
+    }
+    return a.value < b.value;
+}
+
+void quicksort(vector<Element> &arr, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    int pivotIndex = left + (right - left) / 2;
+    Element pivot = arr[pivotIndex];
+
+    int i = left, j = right;
+    while (i <= j) {
+        while (compareValues(arr[i], pivot)) {
+            i++;
+        }
+        while (compareValues(pivot, arr[j])) {
+            j--;
+        }
+        if (i <= j) {
+            mySwap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    quicksort(arr, left, j);
+    quicksort(arr, i, right);
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    srand(time(0));
+    vector<int> A(N);
+    vector<Element> sortedA(N);
+
+    for (int i = 0; i < N; i++) {
+        A[i] = rand() % 101;
+        cout << A[i] << " ";
+        sortedA[i].index = i;
+        sortedA[i].value = A[i];
+    }
+    cout << endl << endl;
+
+    quicksort(sortedA, 0, N - 1);
+
+    vector<int> P(N);
+    for (int i = 0; i < N; i++) {
+        P[sortedA[i].index] = i;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << P[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
